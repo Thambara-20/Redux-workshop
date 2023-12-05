1fe17e78170ee27b4562dc71f73e59ae2b10c89c
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-12-05 19:04:46 +0530

    action creaters, dispatchers implemented

---
+++ index.js
@@ -0,0 +1,56 @@
+const redux = require('redux');
+const createStore= redux.createStore;
+
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTAKE = 'CAKE_RESTAKE';
+
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const restakeCake = () => {
+    return {
+        type: CAKE_RESTAKE,
+        quantity: 5,
+    };
+}
+
+const initialState = {
+    numberOfCakes: 10,
+    };
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+        return {
+            ...state,
+            numberOfCakes: state.numberOfCakes - action.quantity,
+        };
+        case CAKE_RESTAKE:
+        return {
+            ...state,
+            numberOfCakes: state.numberOfCakes + action.quantity,
+        };
+        default:
+        return state;
+    }
+}
+
+const store = createStore(reducer);
+
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+store.dispatch(restakeCake());
+
+unsubscribe();
+
+
